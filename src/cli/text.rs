ca27a1f3d97a9f5825dac5e05358b13b66937343
maer09 <maer09@users.noreply.github.com>
2024-05-01 21:59:35 +0800

    添加加解密的示例

--- src/cli/text.rs
+++ src/cli/text.rs
@@ -13,9 +13,11 @@ use chacha20poly1305::{
 
 // 阅读 chacha20poly1305 文档，了解其使用方法并构建 CLI 对输入文本进行加密 / 解密
 // 要求：
-
 // rcli text encrypt -key"xxx"> 加密并输出 base64
 // rcli text decrypt -key"XXX" >base64 > binary> 解密文本
+// 
+// cargo run encrypt -k AHpveN84mAGJshihuviO8wffk5rqUIYU -i fixtures/text_to_encrypt.txt
+// cargo run text decrypt -k AHpveN84mAGJshihuviO8wffk5rqUIYU -i fixtures/text_to_decrypt.txt
 
 /// Subcommand of Text Command
 #[derive(Debug, Subcommand)]
