3f11fa036adac8fcad83667a101d7b9f113b8598
maer09 <maer09@users.noreply.github.com>
2024-05-02 01:30:26 +0800

    完成jwt验证

---
+++ src/cli/jwt.rs
@@ -0,0 +1,79 @@
+// json web token(jwt) 在用户验证领域经常被用到。请构建一个 CLI 来为给定 sub/aud/exp/… 生成一个 jwt。要求生成的 jwt 可以通过 jwt.io 的验证。
+
+// CLI：
+
+// rcli jwt sign --sub acme --aud device1 --exp 14d
+// rcli jwt verify -t
+
+// cargo run jwt sign -s Goo -a www.google.com -e 1714593571436
+// cargo run jwt verify -t eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJHb28iLCJhdWQiOiJ3d3cuZ29vZ2xlLmNvbSIsImV4cCI6MTcxNDU5MzU3MTQzNn0.CRIoZuLnQ6JUJ9nwHfATCd6PQxVxp4ZWoy32fdoct3VNx9CpIUpKHQIVb8EGfYn0FREy6axobVbmFzC7uBMvRA -a www.google.com
+
+use clap::{Parser, Subcommand};
+use enum_dispatch::enum_dispatch;
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+
+use crate::CmdExecutor;
+
+/// Subcommand of Text Command
+#[derive(Debug, Subcommand)]
+#[enum_dispatch(CmdExecutor)]
+pub enum JwtSubCommand {
+    Sign(TextJwtSignOpts),
+    Verify(TextJwtVerifyopts),
+}
+
+const JWT_SECRET: &[u8] = b"secret";
+
+#[derive(Debug, Parser, Serialize)]
+pub struct TextJwtSignOpts {
+    #[arg(short, long)]
+    sub: String,
+    #[arg(short, long)]
+    aud: String,
+    #[arg(short, long)]
+    exp: u64,
+}
+
+#[derive(Debug, Parser)]
+pub struct TextJwtVerifyopts {
+    #[arg(short, long)]
+    token: String,
+    #[arg(short, long)]
+    aud: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    sub: String,
+    aud: String,
+    exp: u64,
+}
+
+impl CmdExecutor for TextJwtSignOpts {
+    async fn execute(self) -> anyhow::Result<()> {
+        let header = Header::new(Algorithm::HS512);
+        let claims = Claims {
+            sub: self.sub,
+            aud: self.aud,
+            exp: self.exp,
+        };
+        let jwt = encode(&header, &claims, &EncodingKey::from_secret(JWT_SECRET))?;
+        println!("{}", jwt);
+        Ok(())
+    }
+}
+
+impl CmdExecutor for TextJwtVerifyopts {
+    async fn execute(self) -> anyhow::Result<()> {
+        let mut validation = Validation::new(Algorithm::HS512);
+        validation.set_audience(&[self.aud]);
+        let decoded = decode::<Claims>(
+            &self.token,
+            &DecodingKey::from_secret(JWT_SECRET),
+            &validation,
+        )?;
+        println!("{:?}", decoded.claims);
+        Ok(())
+    }
+}
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,25 +1,29 @@
 mod text;
+mod jwt;
 
 use std::path::Path;
 
 use clap::{ Parser, Subcommand};
 use enum_dispatch::enum_dispatch;
 
-pub use self::text::*;
+pub use self::{text::*, jwt::*};
 
 /// Command Set
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
-    pub cmd: Commands
+    pub cmd: Commands,
 }
 
 #[derive(Debug, Subcommand)]
 #[enum_dispatch(CmdExecutor)]
 pub enum Commands {
     #[command(subcommand)]
-    Text(TextSubCommand)
+    Text(TextSubCommand),
+
+    #[command(subcommand)]
+    Jwt(JwtSubCommand)
 }
 
 /// Verify if the filename input is exists.
